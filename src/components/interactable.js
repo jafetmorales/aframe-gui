f6bb28069973b466cfe2db22ee8f26264489fbfb
Ubuntu <jafet.aaron.morales@gmail.com>
2018-10-03 05:41:17 +0000

    Testing

--- src/components/interactable.js
+++ src/components/interactable.js
@@ -1,50 +1,35 @@
 AFRAME.registerComponent('gui-interactable', {
     schema: {
-        clickAction: { type: 'string' },
-        hoverAction: { type: 'string' },
-        keyCode: { type: 'number', default: null },
+        clickAction: {type: 'string'},
+        hoverAction: {type: 'string'},
+        keyCode: {type: 'number', default: null},
     },
-    init: function() {
+    init: function () {
         var _this = this;
         var data = this.data;
         var el = this.el;
 
-        this.keyHandler = function(event) {
-            if (event.keyCode == data.keyCode) {
-                console.log("key press by gui-interactable : " + data.keyCode);
-                el.emit('click');
-            }
-            // event.preventDefault();
+        if(data.keyCode){
+            window.addEventListener("keydown", function (event) {
+                if(event.keyCode == data.keyCode){                  
+                    console.log("key press by gui-interactable : " + data.keyCode);
+                    el.emit('click');
+                }
+                // event.preventDefault();
+            }, true);
         }
-        if (data.keyCode) {
-            window.addEventListener("keydown", this.keyHandler, true);
-        }
-
-
-        // if(data.keyCode){
-        //     window.addEventListener("keydown", function (event) {
-        //         if(event.keyCode == data.keyCode){                  
-        //             console.log("key press by gui-interactable : " + data.keyCode);
-        //             el.emit('click');
-        //         }
-        //         event.preventDefault();
-        //     }, true);
-        // }
     },
-    update: function() {},
-    tick: function() {},
-    remove: function() {
-        console.log("AM REMOVED BRO")
-        // window.removeEventListener("keydown", this.keyHandler, true);
+    update: function () {
     },
-    pause: function() {
-        console.log("AM PAUSING BRO")
-        // window.removeEventListener("keydown", this.keyHandler, true);
+    tick: function () {
     },
-    play: function() {
-        console.log("AM PLAYING BRO")
+    remove: function () {
     },
-    setClickAction: function(action) {
+    pause: function () {
+    },
+    play: function () {
+    },
+    setClickAction: function (action) {
         this.data.clickAction = action; //change function dynamically
     },
 });
